c72be3b8ab9fd4547a10c84883f7e1191dc9e1f0
ehsanabdikhani <abdikhaniehsan@gmail.com>
2026-06-15 12:25:24 +0330

    urls.py updated

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.contrib.auth.hashers import make_password
+from .models import User, Role
+from .serializers import UserSerializer, RoleSerializer
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    
+    @action(detail=False, methods=['get'])
+    def me(self, request):
+        serializer = self.get_serializer(request.user)
+        return Response(serializer.data)
+    
+    @action(detail=False, methods=['post'])
+    def register(self, request):
+        data = request.data.copy()
+        data['password'] = make_password(data.get('password'))
+        serializer = self.get_serializer(data=data)
+        if serializer.is_valid():
+            user = serializer.save()
+            try:
+                student_role = Role.objects.get(name='student')
+                user.roles.add(student_role)
+            except Role.DoesNotExist:
+                pass
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class RoleViewSet(viewsets.ModelViewSet):
+    queryset = Role.objects.all()
+    serializer_class = RoleSerializer
+    permission_classes = [permissions.IsAdminUser]
