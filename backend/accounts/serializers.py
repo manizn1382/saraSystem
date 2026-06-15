a0db927a9098fc8194e5474f92057bff76235505
ehsanabdikhani <abdikhaniehsan@gmail.com>
2026-06-15 12:20:48 +0330

    added serializers.py to accounts serviece

---
+++ backend/accounts/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import User, Role, Permission
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'first_name', 'last_name', 'national_id', 
+                  'student_id', 'phone', 'gender', 'is_active', 'is_verified']
+        read_only_fields = ['id', 'created_at', 'updated_at']
+
+class RoleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Role
+        fields = '__all__'
