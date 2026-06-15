1d315befc423aa8765efb3d3dc7ae6368259ad54
ehsanabdikhani <abdikhaniehsan@gmail.com>
2026-06-15 14:14:02 +0330

    accounts test-cases added \n bug fixed in urls

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -8,4 +8,8 @@ router.register(r'roles', RoleViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('register/', UserViewSet.as_view({'post': 'register'}), name='register'),
+    path('me/', UserViewSet.as_view({'get': 'me'}), name='me'),
+    path('update-profile/', UserViewSet.as_view({'put': 'update_profile', 'patch': 'update_profile'}), name='update_profile'),
+    path('change-password/', UserViewSet.as_view({'post': 'change_password'}), name='change_password'),
 ]
