32a414cbca3028b6f3c3739efd52fcb595d8ee86
ehsanabdikhani <abdikhaniehsan@gmail.com>
2026-06-15 13:02:13 +0330

    backend urls.py updated

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,16 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from accounts.views import UserViewSet, RoleViewSet
-
-router = DefaultRouter()
-router.register(r'users', UserViewSet)
-router.register(r'roles', RoleViewSet)
+from rest_framework_simplejwt.views import TokenRefreshView
+from accounts.views import CustomTokenObtainPairView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
-    path('api/auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/accounts/', include('accounts.urls')),
+    path('api/auth/login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
