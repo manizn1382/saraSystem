99db7efbcf799212f2bc93a83d2c7284a4295734
ehsanabdikhani <abdikhaniehsan@gmail.com>
2026-06-16 17:44:37 +0330

    admin.py updated

--- backend/allocations/admin.py
+++ backend/allocations/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import AccommodationRequest, BedAssignment
 
-# Register your models here.
+@admin.register(AccommodationRequest)
+class AccommodationRequestAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user_id', 'semester', 'status', 'request_date']
+    list_filter = ['status', 'semester']
+
+@admin.register(BedAssignment)
+class BedAssignmentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user_id', 'bed_id', 'status', 'start_date']
+    list_filter = ['status']
