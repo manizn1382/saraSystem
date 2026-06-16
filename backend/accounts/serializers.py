ca9cbbf3af2c4f71712c023185f9c197fbc63deb
ehsanabdikhani <abdikhaniehsan@gmail.com>
2026-06-16 21:54:31 +0330

    bug fixed at serializers

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -5,10 +5,8 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'email', 'first_name', 'last_name', 'national_id', 
-                  'student_id', 'phone', 'gender', 'is_active', 'is_verified']
+                  'student_id', 'phone', 'gender', 'is_active', 'is_verified', 'password']
         read_only_fields = ['id', 'created_at', 'updated_at']
-
-class RoleSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Role
-        fields = '__all__'
+        extra_kwargs = {
+            'password': {'write_only': True}  # Password is write-only, never returned
+        }
