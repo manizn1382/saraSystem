cda087922d0ed6ae359cad75b6c0110d3967d1dd
ehsanabdikhani <abdikhaniehsan@gmail.com>
2026-06-17 22:54:13 +0330

    mysql server config done

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -62,9 +62,13 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'accounts_database',
+        'USER': 'accounts_user',
+        'PASSWORD': 'accounts_password',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    },
 }
 
 AUTH_PASSWORD_VALIDATORS = [
