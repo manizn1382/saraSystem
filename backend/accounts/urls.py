248ad5e07afccf40a98cefa9dd2ba063d8fc1a9c
amir zameni <manizm1382@gmail.com>
2026-06-19 14:19:23 +0330

    add two api to get and refresh jwt token

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet, RoleViewSet
+from .views import UserViewSet, RoleViewSet, CustomTokenObtainPairView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
@@ -8,6 +9,8 @@ router.register(r'roles', RoleViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('getToken/', CustomTokenObtainPairView.as_view(), name="get-Token"),
+    path('refreshToken/', TokenRefreshView.as_view(),name="refresh-token"),
     path('register/', UserViewSet.as_view({'post': 'register'}), name='register'),
     path('me/', UserViewSet.as_view({'get': 'me'}), name='me'),
     path('update-profile/', UserViewSet.as_view({'put': 'update_profile', 'patch': 'update_profile'}), name='update_profile'),
